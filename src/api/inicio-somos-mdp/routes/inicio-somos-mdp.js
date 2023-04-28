ac6b367c5cbb9419a16885c495c15b8d425a0123
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-04-27 22:44:29 -0500

    feat(Strapi Backend): creation of apis for the home page

---
+++ src/api/inicio-somos-mdp/routes/inicio-somos-mdp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * inicio-somos-mdp router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::inicio-somos-mdp.inicio-somos-mdp');
