d61013647e5515d6b66eeddc3af806896bf87a9a
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-11 18:28:55 -0500

    feat: add model for contctinfoconsulting

---
+++ src/api/contact-info-consulting/controllers/contact-info-consulting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-info-consulting controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::contact-info-consulting.contact-info-consulting');
