d2527bb78c8a2c013d1b70f2e444c93e63babd53
JoelWences <joelbejarespinoza@gmail.com>
2023-05-11 20:47:58 -0500

    feat: adding API to software development, software testing and consultancy

---
+++ src/api/model-service-child/routes/model-service-child.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * model-service-child router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::model-service-child.model-service-child');
