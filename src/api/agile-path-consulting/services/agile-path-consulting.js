7b43495ed720affc5f1a08c6c8b08b7fa77ac16d
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-12 13:05:47 -0500

    feat: add model tapeServices

---
+++ src/api/agile-path-consulting/services/agile-path-consulting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * agile-path-consulting service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::agile-path-consulting.agile-path-consulting');
---
+++ src/api/tape-services-consulting/routes/tape-services-consulting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tape-services-consulting router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tape-services-consulting.tape-services-consulting');
