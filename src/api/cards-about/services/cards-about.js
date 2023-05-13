5f2ea0d5f6665dcd29a962da5d56bd71b15f165b
Aldair Chuman <u202017200@upc.edu.pe>
2023-05-13 00:30:59 -0500

    feat: add subpage vision and purpose on strappi

---
+++ src/api/cards-about/services/cards-about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cards-about service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cards-about.cards-about');
