a27b07d99fc33804eaa535000a66b9b1b3e1ee6a
Nadia Tinoco <lorenanadia2002@gmail.com>
2023-05-13 21:51:34 -0500

    fix: fix conflicts of merge

---
+++ src/api/content-home/routes/content-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-home router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::content-home.content-home');
