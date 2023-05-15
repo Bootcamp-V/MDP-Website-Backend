eb7636665c82c803db718c69db157d3795f1acea
Nadia Tinoco <lorenanadia2002@gmail.com>
2023-05-15 18:12:31 -0500

    feat: add content types of rights-arco subpage

---
+++ src/api/description-rights-arco/routes/description-rights-arco.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * description-rights-arco router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::description-rights-arco.description-rights-arco');
