0917be3bb24f395988bfcc3f4df1f3e799fd1f85
Aldair Chuman <u202017200@upc.edu.pe>
2023-05-16 23:57:32 -0500

    feat: complete union strappi with form of complaint book

---
+++ src/api/complaints-book-info/routes/complaints-book-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * complaints-book-info router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::complaints-book-info.complaints-book-info');
