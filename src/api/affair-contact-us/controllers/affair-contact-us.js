d9f2d2a54fbb10184a4e81e9c6b18a1077e2ba73
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-16 19:43:10 -0500

    feat add model for post api contactUsForm

---
+++ src/api/affair-contact-us/controllers/affair-contact-us.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * affair-contact-us controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::affair-contact-us.affair-contact-us');
---
+++ src/api/affair-contact-us/routes/affair-contact-us.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * affair-contact-us router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::affair-contact-us.affair-contact-us');
