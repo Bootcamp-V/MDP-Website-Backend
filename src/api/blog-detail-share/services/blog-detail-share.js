e1762fe3d7ae3e4d2a59e8114390671aef833051
JoelWences <joelbejarespinoza@gmail.com>
2023-06-01 10:50:04 -0500

    feat: add model Blog Detail Sharet

---
+++ src/api/blog-detail-share/services/blog-detail-share.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-detail-share service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::blog-detail-share.blog-detail-share');
